daf6bc533da4dd75d702d2c3e7d0bb77955c061b
Marek Żarnowski <marek1840@gmail.com>
2022-02-17 14:44:57 +0100

    Add PKI module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,5 +6,8 @@ findProject(":infra:buffer")?.name = "buffer"
 include("infra:fs:fileclerk")
 findProject(":infra:fs:fileclerk")?.name = "fileclerk"
 
+include("infra:pki")
+findProject(":infra:pki")?.name = "pki"
+
 include("os:executable:elf")
 findProject("os:executable:elf")?.name = "elf-executable"
